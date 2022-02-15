8f63bc08c4fa0c6c38d82dedc7a2ea123ab2f369
NathanLoranceMvb <matslavarock@gmail.com>
2022-02-15 13:30:36 +0100

    2.6

---
+++ 2-Variabelen-en-datatype/2.6 - gettype() 2/index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+  $br="<br>";
+  $regelTekst1="Dit is de string";
+  echo $regelTekst1;
+  echo $br;
+  echo gettype($regelTekst1);
+ 
+  echo $br;
+  echo $br;
+
+  $regel = 5985;
+  echo ($regel);
+  echo $br;
+  echo gettype($regel);
+
+  echo $br;
+  echo $br;
+
+  $x = 10.365;
+  echo($x);
+  echo $br;
+  var_dump ($x);
+
+  echo $br;
+  echo $br;
+
+  $true = true;
+  $false = false;
+  echo $true;
+  echo $br;
+  echo gettype($false);
+  echo $br;
+  echo $false;
+  echo $br;
+  echo gettype($true);
+
+  echo $br;
+  echo $br;
+
+  $cars = array("Volvo","BMW","Toyota");
+var_dump($cars);
+echo $br;
+echo gettype($cars);
+?>
+
+</body>
+</html> 
