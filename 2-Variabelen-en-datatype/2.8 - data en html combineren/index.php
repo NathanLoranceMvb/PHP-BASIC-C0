e49ea8eb8a27ee17363a8e6795528040e76c765d
NathanLoranceMvb <matslavarock@gmail.com>
2022-02-16 09:21:36 +0100

    2.8

---
+++ 2-Variabelen-en-datatype/2.8 - data en html combineren/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$voornaam1= "Mats";
+$voornaam2= "Jim";
+$voornaam3= "Greg";
+
+   echo "<li>$voornaam1</li><li>$voornaam2</li><li>$voornaam3</li>";
+?>
+
+</body>
+</html> 
