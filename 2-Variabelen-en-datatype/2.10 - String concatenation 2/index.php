780c903cc66c32d6fd07848529178fc1750b9751
NathanLoranceMvb <matslavarock@gmail.com>
2022-02-16 11:43:11 +0100

    2.10

---
+++ 2-Variabelen-en-datatype/2.10 - String concatenation 2/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+    $item = "<li>glass</li>";
+    $item .= "<li>baloon</li>";
+    $item .= "<li>knve</li>";    
+
+    echo $item;
+
+?>
+
+</body>
+</html> 
