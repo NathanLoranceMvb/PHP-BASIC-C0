d09346f40832c72a3974de01c4ae364dde31aa4c
NathanLoranceMvb <90605175+NathanLoranceMvb@users.noreply.github.com>
2022-02-21 15:56:25 +0100

    3.1

---
+++ 3-Arrays/3.1 - Array variabele/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$myMovies = ["Spiderman", "Batman", "The Joker"];
+echo "<li>$myMovies[0]</li><li>$myMovies[1]</li><li>$myMovies[2]</li>";
+//het staat leterlijk in de opdracht dat het in deze volgorde moet in deze volgorde
+?> 
+
+</body>
+</html> 
+
