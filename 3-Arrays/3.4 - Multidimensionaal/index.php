a9e09ed55a4c42008beece28c647fc8e43cdf7eb
NathanLoranceMvb <90605175+NathanLoranceMvb@users.noreply.github.com>
2022-02-24 14:19:02 +0100

    3.4

--- 3-Arrays/3.4 - Multidimensionaal/index.php
+++ 3-Arrays/3.4 - Multidimensionaal/index.php
@@ -3,7 +3,14 @@
 <body>
 
 <?php
+    $abcGetallen = ['a', 'b', 'c', 24,56,78,90];
+    $getalnummer = "a56 c90";
 
+    print_r($abcGetallen);
+    $br="<br>"; echo $br;
+    echo $getalnummer
+    
+//ik heb hier nog uitleg bij nodig want ik weet niet zeker of ik het goed heb gedaan.
 ?>
 
 </body>
