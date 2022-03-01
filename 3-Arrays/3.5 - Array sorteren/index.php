6dbb1995a5ba2cb1b988054bf25d30c0b0dc7293
NathanLoranceMvb <matslavarock@gmail.com>
2022-03-01 12:38:04 +0100

    3.5

--- 3-Arrays/3.5 - Array sorteren/index.php
+++ 3-Arrays/3.5 - Array sorteren/index.php
@@ -3,7 +3,17 @@
 <body>
 
 <?php
+    $getallen =  [676,5345,78657,3,46,21,47,];
+    asort($getallen);
 
+    $laag_hoog = $getallen;
+    print_r($laag_hoog);
+
+    $br="<br>"; echo $br; echo $br;
+    
+    arsort($getallen);
+    $hoog_laag = $getallen;
+    print_r($hoog_laag);
 ?>
 
 </body>
