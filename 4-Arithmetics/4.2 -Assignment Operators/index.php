e945f773be2ba800252a7584972b7828f7af175a
NathanLoranceMvb <matslavarock@gmail.com>
2022-03-02 10:22:02 +0100

    4.2

--- 4-Arithmetics/4.2 -Assignment Operators/index.php
+++ 4-Arithmetics/4.2 -Assignment Operators/index.php
@@ -3,7 +3,19 @@
 <body>
 
 <?php
-
+    $getal1 = 21;
+    $getal2 = 19;
+    echo $getal1 + $getal2;
+   
+    $br="<br>"; echo $br;
+    echo $getal1 -= $getal2;
+    
+    echo $br;
+    echo $getal1 *= $getal2;
+    
+    echo $br;
+    echo $getal1 /= $getal2;
+    
 ?>
 
 </body>
