632ba42601fa454c9dc2af4734f1e492f01e172a
NathanLoranceMvb <matslavarock@gmail.com>
2022-03-04 10:50:36 +0100

    5.2

---
+++ 5-Conditional_Statements/5.2 - if conditional/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+
+    $test1 = 10;
+    $test2 = 6;
+    if($test1 == $test2) echo "gelijk";
+    $br="<br>"; echo $br;
+
+    if($test1 != $test2) echo "ongelijk";
+
+    echo $br;
+    if($test1 === $test2) echo "identiek";
+
+    echo $br;
+    if($test1 > $test2) echo "test1 is groter dan test2";
+
+    echo $br;
+    if($test1 < $test2) echo "test1 is kleiner dan test2";
+    
+    echo $br;
+    if($test1 >= $test2) echo "test1 is groter of gelijk aan test2";
+    
+    echo $br;
+    if($test1 <= $test2) echo "test1 is kleiner of gelijk aan test2";
+?>
+
+</body>
+</html> 
