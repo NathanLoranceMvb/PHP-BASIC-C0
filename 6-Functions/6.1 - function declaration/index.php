bd5bc355687e4b66ee2c8910261250aa2abd29da
NathanLoranceMvb <matslavarock@gmail.com>
2022-03-08 13:51:07 +0100

    6.1

---
+++ 6-Functions/6.1 - function declaration/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+function zetWoordenAanElkaar(){
+    $woordje = 'Hallo ';
+    $zinnetje = 'mooie blauwe planeet!';
+    $heleZin =  $woordje . $zinnetje;
+    echo $heleZin;
+}
+
+zetWoordenAanElkaar();
+
+?>
+
+</body>
+</html> 
