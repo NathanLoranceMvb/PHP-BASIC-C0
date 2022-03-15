9324f817c983965b4458ab55f567604200e8c81f
NathanLoranceMvb <matslavarock@gmail.com>
2022-03-15 10:27:16 +0100

    7.2

---
+++ 7-Working with files/7.2 - require/includes/database.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+</head>
+<body>
+
+    <div class="menu">
+        <?php include 'menu.php'; ?>
+    </div>
+
+</body>
+</html>
---
+++ 7-Working with files/7.2 - require/menu.php
@@ -0,0 +1,14 @@
+<html>
+<body>
+<?php
+$names = ["Gregory","Jim","Sam"];
+
+echo"<ul>" ;
+echo "<li>menu-item1</li>";
+echo "<li>menu-item2</li>";
+echo "<li>menu-item3</li>";
+echo"</ul>";
+
+?>
+</body>
+</html>
